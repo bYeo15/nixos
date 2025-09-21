2e251004eeca0364808d246d01c6edc61f79394a
Ben Y. <bjy777101@gmail.com>
2025-09-21 17:00:11 +1000

    Added proper agenix loading to host nixpad

---
+++ hosts/nixpad/system/age.nix
@@ -0,0 +1,5 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+    age.identityPaths = [ "/home/ben/.ssh/ben_user_id" ];
+}
--- hosts/nixpad/system/default.nix
+++ hosts/nixpad/system/default.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+        ./age.nix
         ./bluetooth.nix
         ./boot.nix
         ./display.nix
