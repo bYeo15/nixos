4406bb92921f25d52896ff9ff790e3ab082ec564
Ben Y. <bjy777101@gmail.com>
2025-09-30 21:42:51 +1000

    Added rpi brcmfmac modprobe config

--- hosts/nixpi/system/boot.nix
+++ hosts/nixpi/system/boot.nix
@@ -4,4 +4,9 @@
     boot.kernel.sysctl = {
         "net.ipv4.conf.default.forwarding" = 1;
     };
+
+    boot.extraModprobeConfig = ''
+        options brcmfmac feature_disable=0x282000
+        options brcmfmac roamoff=1
+    '';
 }
