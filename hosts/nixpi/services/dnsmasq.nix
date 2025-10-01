217ebd7c22d534fb57cdd52b80f7415cc9bb44c4
Ben Y. <bjy777101@gmail.com>
2025-10-01 20:14:08 +1000

    Added rpi as router for dnsmasq

--- hosts/nixpi/services/dnsmasq.nix
+++ hosts/nixpi/services/dnsmasq.nix
@@ -9,6 +9,7 @@
             port = 0;
             # 2-10 are reserved for static assignment
             dhcp-range = [ "10.0.0.10,10.0.0.254,255.255.255.0,6h" ];
+            dhcp-option = [ "3,10.0.0.1" ];
             dhcp-host = config.netconn.dhcpMappings;
             dhcp-authoritative = true;
         };
