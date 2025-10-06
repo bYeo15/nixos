23e53967ebad7478c650f21df1b29abaea5122d6
Ben Y. <bjy777101@gmail.com>
2025-10-06 20:04:49 +1100

    Added ssh key for gaming & node, added ssh secrets for nixpc

--- users/gaming.nix
+++ users/gaming.nix
@@ -5,5 +5,9 @@
         isNormalUser = true;
 
         shell = pkgs.bash;
+
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuhigSbK+Ftd99A4OWm8L2bT4wbxbe01zcW9lkxUVYw"
+        ];
     };
 }
