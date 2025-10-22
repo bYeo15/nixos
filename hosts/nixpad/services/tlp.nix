4f3e3ad2a171f89665fa8d66337d94b7fc4ac73b
Ben Y. <bjy777101@gmail.com>
2025-10-22 20:29:44 +1100

    Added tlp battery thresholds to nixpad

--- hosts/nixpad/services/tlp.nix
+++ hosts/nixpad/services/tlp.nix
@@ -7,8 +7,11 @@
             CPU_SCALING_GOVERNOR_ON_AC = "performance";
             CPU_SCALING_GOVERNOR_ON_BAT = "performance";
 
-            CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
             CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+            START_CHARGE_THRESH_BAT0 = 75;
+            STOP_CHARGE_THRESH_BAT0 = 80;
         };
     };
 }
